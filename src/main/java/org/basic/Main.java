231835d32d97864ffb7e5a4f566d20b31f16128a
王元帅 <wangys@koal.com>
2023-03-28 10:52:19 +0800

    Initial commit

---
+++ src/main/java/org/basic/Main.java
@@ -0,0 +1,13 @@
+package org.basic;
+
+/**
+ * @author wys
+ * @date 2023/3/28 10:49
+ **/
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+
+}
